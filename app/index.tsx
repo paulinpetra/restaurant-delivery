72e5a339ba8e35dd3cc8c616155e2d2c33511adb
Petra Paulin <petra.oster.paulin@gmail.com>
2025-11-20 10:36:01 +0100

    implement homescreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,37 @@
 import "./global.css"
 import {SafeAreaView} from "react-native-safe-area-context";
 import {FlatList, Pressable, View, Text, Image} from "react-native";
-import {offers} from "@/constants";
+import {images, offers} from "@/constants";
 import {Fragment} from "react";
+import cn from 'clsx';
 
 export default function Index() {
     return (
-       <SafeAreaView>
+       <SafeAreaView className="flex-1 bg-white">
            <FlatList
                data={offers}
                renderItem={({item, index}) => {
+                   const isEven : boolean = index % 2 === 0;
+
                    return (
                        <View>
-                           <Pressable className= "offer-card" style={{backgroundColor: item.color}}>
+                           <Pressable className= {cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')} style={{backgroundColor: item.color}}>
                                {({pressed}) => (
                                    <Fragment>
                                        <View className={"h-full w-1/2"}>
                                            <Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
+                                       </View>
+                                       <View className={cn("offer-card__info", isEven ? 'pl-10' : 'pr-10')}>
+                                           <Text className="h1-bold text-white leading-tight">
+                                               {item.title}
+                                           </Text>
+                                           <Image
+                                               source={images.arrowRight}
+                                               className="size-10"
+                                               resizeMode="contain"
+                                               tintColor="#ffffff"
+                                           />
+
 
                                        </View>
 
@@ -26,6 +41,7 @@ export default function Index() {
                        </View>
                    )
                }}
+               contentContainerClassName="pb-28 px-5"
            />
 
        </SafeAreaView>
